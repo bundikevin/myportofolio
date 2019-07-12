ddd48a7405d401c71c210e1f6b8f42c8751a54a7
bundikevin <bundikevinmakori@gmail.com>
2019-07-12 09:10:25 +0300

    React App

---
+++ src/components/landingpage.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Landing extends Component {
+  render() {
+  	return (
+  		<div style={{width: '100%', margin: 'auto'}}>
+  			<Grid className="landing-grid">
+  				<Cell col={12}>
+  				
+  					<img 
+  					src="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg" 
+  					alt="avatar" 
+  					className="avatar-img"
+  					/>
+
+  					<div className="banner-text">
+  						<h1>Full Stack Web Developer</h1>
+
+  						<hr/>
+
+  						<p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+
+  						<div className="social-links">
+
+  							<a href="https://www.linkedin.com/in/bundi-makori-948323137/" rel="noopener noreferrer" target="_blank">
+  								<i className="fa fa-linkedin-square"  aria-hidden="true"/>
+  								<h6>Linkedin</h6>
+  							</a>
+
+  							<a href="https://github.com/bundikevin" rel="noopener noreferrer" target="_blank">
+  								<i className="fa fa-github-square"  aria-hidden="true"/>
+  								<h6>Github</h6>
+  							</a>
+
+  							<a href="https://www.freecodecamp.org/settings" rel="noopener noreferrer" target="_blank">
+  								<i className="fa fa-free-code-camp"  aria-hidden="true"/>
+  								<h6>Free-Code-Camp</h6>
+  							</a>
+
+  							<a href="https://www.youtube.com/channel/UC1GPACxkoXPrfj1MkwUWtjQ?view_as=subscriber" rel="noopener noreferrer" target="_blank">
+  								<i className="fa fa-youtube-square"  aria-hidden="true"/>
+  								<h6>Youtube</h6>
+  							</a>
+
+  						</div>
+  					</div>
+
+  				</Cell>
+
+  			</Grid>
+
+  		</div>
+  		
+  		)
+  }
+}
+export default Landing;
---
+++ src/components/projects.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+
+
+class Projects extends Component {
+	constructor(props){
+		super(props);
+		this.state = { activeTab: 0};
+	}
+	toggleCategories(){
+
+		if(this.state.activeTab === 0){
+		return(
+<div className="projects-grid">
+	{/*Project 1*/}
+			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+				<CardTitle style={{color: '#111', height: '176px', background: 'URL(https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png) center / cover'}}>React Project</CardTitle>
+				<CardText>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</CardText>
+				<CardActions border>
+					<Button colored>Github</Button>
+					<Button colored>CodePen</Button>
+					<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: ' #111'}}>
+					<IconButton name="share"/>
+				</CardMenu>
+			</Card>
+
+			{/*Project 2*/}
+			<Card shadow={5} style={{minWidth: '450', margin: '50px'}}>
+				<CardTitle style={{color: '#111', height: '176px', background: 'URL(https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png) center / cover'}}>React Project</CardTitle>
+				<CardText>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</CardText>
+				<CardActions border>
+					<Button colored>Github</Button>
+					<Button colored>CodePen</Button>
+					<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: ' #111'}}>
+					<IconButton name="share"/>
+				</CardMenu>
+			</Card>
+
+		{/*Project 3*/}
+			<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+				<CardTitle style={{color: '#111', height: '176px', background: 'URL(https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png) center / cover'}}>React Project</CardTitle>
+				<CardText>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</CardText>
+				<CardActions border>
+					<Button colored>Github</Button>
+					<Button colored>CodePen</Button>
+					<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: ' #111'}}>
+					<IconButton name="share"/>
+				</CardMenu>
+			</Card>
+	</div>
+			)
+		}
+		else if(this.state.activeTab === 1){
+		return(
+				<div>
+				<h1>This is Angular Project</h1>
+				</div>
+			)
+		}
+		else if(this.state.activeTab === 2){
+		return(
+				<div>
+				<h1>This is VueJs Project</h1>
+				</div>
+			)
+		}
+		else if(this.state.activeTab === 3){
+		return(
+				<div>
+				<h1>This is MongoDB Project</h1>
+				</div>
+			)
+		}
+
+	}
+	
+  render(){
+  	return(
+  			<div className="category-tabs">
+               <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+               		<Tab>React</Tab>
+               		<Tab>Angular</Tab>
+               		<Tab>VueJs</Tab>
+               		<Tab>MongoDB</Tab>
+               </Tabs> 
+
+               <section className="projects-grid">
+               <Grid >
+               		<Cell col={12}>
+               			<div className="content">
+               				{this.toggleCategories()}
+               			</div>
+               		</Cell>
+               </Grid>
+               		
+               </section>
+            </div>    
+  		);
+  }
+}
+export default Projects;
